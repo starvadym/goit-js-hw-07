668c0bd67629f5ea7f913916400af1e059638131
Vadym Starchenko <starvadim@ukr.net>
2022-02-05 09:58:49 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,7 +4,6 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const refGallery = document.querySelector('.gallery');
-let instance;
 
 const markup = galleryItems.map(({ preview, original, description }) => {
     return `
