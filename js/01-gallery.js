657bb9320d50e7c2391d3a25693ea3f08f0b8c22
Vadym Starchenko <starvadim@ukr.net>
2022-02-05 09:29:47 +0200

    Last

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,62 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refGallery = document.querySelector('.gallery');
+let instance;
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+   </a>
+    </div>`
+})
+    .join('');
+
+refGallery.insertAdjacentHTML('beforeend', markup);
+
+
+
+
+function openModal(event) {
+    event.preventDefault();
+    if (event.target.tagName !== "IMG") {
+        return;
+    }
+    const url = event.target.dataset.source;
+    instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${url}" width= "1000"/>
+    </div>
+` ,);
+    instance.show();
+    const refModal = document.querySelector(".modal");
+    refModal.addEventListener('click', closeModal);
+    document.addEventListener('keydown', onEscPress);
+}
+
+function closeModal() {
+  document.removeEventListener('keydown', onEscPress);
+  instance.close();
+}
+
+// 2. Добавляем слушателя события на родителя галереи по клику
+
+refGallery.addEventListener('click', openModal);
+
+
+function onEscPress(event) {
+    if (event.code !== 'Escape') {
+        return;
+    }
+    closeModal();
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refGallery = document.querySelector('.gallery');
+let instance;
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+})
+    .join('');
+
+refGallery.insertAdjacentHTML('beforeend', markup);
+
+const lightBoxOptions = {
+ captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionDelay: 250,
+};
+
+let lightbox = new SimpleLightbox('.gallery__item', lightBoxOptions);
