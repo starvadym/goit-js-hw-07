03b78a7009930d68a960e478db6ade2dc3003bbb
Vadym Starchenko <starvadim@ukr.net>
2022-02-05 09:45:17 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,9 +10,9 @@ const markup = galleryItems.map(({ preview, original, description }) => {
     return `
   <a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`
-})
-    .join('');
+  </a>`
+}).join('');
+
 
 refGallery.insertAdjacentHTML('beforeend', markup);
 
